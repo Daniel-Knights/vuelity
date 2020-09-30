ea9c22dd233ee5b2b5e4b41962ad37bb96d17cff
Daniel Knights <danknights95@gmail.com>
2020-09-30 16:30:04 +0100

    Dev environment fix

--- src/main.js
+++ src/main.js
@@ -1,32 +1,3 @@
-import { createApp } from 'vue';
-import App from '@/App.vue';
-
-import DKButton from '@/components/Button.vue';
-import DKNavbar from '@/components/Navbar.vue';
-import DKPopup from '@/components/Popup.vue';
-import DKPagination from '@/components/Pagination.vue';
-import DKVideo from '@/components/Video/Video.vue';
-import DKSearchbar from '@/components/Searchbar/Searchbar.vue';
-import DKTooltip from '@/components/Tooltip.vue';
-import DKHoverbox from '@/components/Hoverbox.vue';
-
-const app = createApp(App);
-
-const Components = {
-    DKButton,
-    DKNavbar,
-    DKPopup,
-    DKPagination,
-    DKVideo,
-    DKSearchbar,
-    DKTooltip,
-    DKHoverbox,
-};
-
-Object.keys(Components).forEach(name => {
-    app.component(name, Components[name]);
-});
-
-export default Components;
+import { app } from './components';
 
 app.mount('#app');
