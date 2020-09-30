6e6acccf0f8371fdf81a99b037df87a2f6333f64
Daniel Knights <danknights95@gmail.com>
2020-09-30 17:38:32 +0100

    Inline CSS

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = {
     outputDir: path.resolve(__dirname, './dist'),
     css: {
+        extract: false,
         loaderOptions: {
             sass: {
                 additionalData: `@import "@/sass/app.scss";`,
